8121e85894d9035b7361423532120b81060bc9b9
Lawrence Luo <luolawrence1@gmail.com>
2024-10-23 14:26:20 -0700

    edited main page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="main-container">
       <header>
         <h1>PerfectRhythm</h1>
-        <p>Coming soon to Android and iOS devices.</p>
+        <p>Coming soon to iOS devices.</p>
       </header>
 
       <body>
         <div className="button-container">
           <a className="action-button" href='privacyPolicy.html' id="privacy-policy">Privacy Policy</a>
           <a className="action-button" href='termsAndConditions.html' id="terms-and-conditions">Terms And Conditions</a>
-          <a className="action-button" href="mailto:newmodeapps@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a>
+          {/* <a className="action-button" href="mailto:newmodeapps@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a> */}
         </div>
       </body>
     </div>
