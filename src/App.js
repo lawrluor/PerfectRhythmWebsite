dc07c019c1bdc8bd59452d19db59004f6dc7a31b
Lawrence Luo <luolawrence1@gmail.com>
2024-10-23 14:04:12 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './reset.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <header>
+        <h1>PerfectRhythm</h1>
+        <p>Coming soon to Android and iOS devices.</p>
+      </header>
+
+      <body>
+        <div className="button-container">
+          <a className="action-button" href='privacyPolicy.html' id="privacy-policy">Privacy Policy</a>
+          <a className="action-button" href='termsAndConditions.html' id="terms-and-conditions">Terms And Conditions</a>
+          <a className="action-button" href="mailto:pledgeofpeaceapp@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a>
+        </div>
+      </body>
+    </div>
+  );
+}
+
+export default App;
