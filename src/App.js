c337f250c1e057082c150cc529be4d6b4e846673
Lawrence Luo <luolawrence1@gmail.com>
2024-10-23 14:07:57 -0700

    changed names

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <div className="button-container">
           <a className="action-button" href='privacyPolicy.html' id="privacy-policy">Privacy Policy</a>
           <a className="action-button" href='termsAndConditions.html' id="terms-and-conditions">Terms And Conditions</a>
-          <a className="action-button" href="mailto:pledgeofpeaceapp@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a>
+          <a className="action-button" href="mailto:newmodeapps@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a>
         </div>
       </body>
     </div>
