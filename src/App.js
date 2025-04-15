429d9898e66e1e05f3728106bf9b24c02ab73a90
Lawrence Luo <luolawrence1@gmail.com>
2025-04-15 02:01:19 -0700

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import './App.css';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import TermsAndConditions from './TermsAndConditions';
+import PrivacyPolicy from './PrivacyPolicy';
+
 import './reset.css';
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <nav className="nav-home">
+        {/* <a className="nav-link" href='about.html' id="about">about</a> */}
+        <Link to="/privacy-policy" className="nav-link" id="privacy-policy">privacy policy</Link>
+        <Link to="/terms-and-conditions" className="nav-link" id="terms-and-conditions">terms and conditions</Link>
+      </nav>
+
+      <section className="main-container">
+        <header>
+          <h1>perfect pulse</h1>
+
+          <div className="description-container">
+            <p>available now on iPhone and iPad</p>
+          </div>
+        </header>
+
+        <div className="button-container">
+          <a className="action-button" href="https://apps.apple.com/us/app/perfect-rhythms/id6744438772" target="_blank" rel="noopener noreferrer">get it on the app store</a>
+        </div>
+
+        <footer className="footer">
+        </footer>
+      </section>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="main-container">
-      <header>
-        <h1>perfect pulse</h1>
-        <p>coming soon to iPhone and iPad.</p>
-      </header>
-
-      <body>
-        <div className="button-container">
-          <a className="action-button" href='privacyPolicy.html' id="privacy-policy">Privacy Policy</a>
-          <a className="action-button" href='termsAndConditions.html' id="terms-and-conditions">Terms And Conditions</a>
-          {/* <a className="action-button" href="mailto:newmodeapps@gmail.com?subject=Delete My Data&body=Please delete the data associated with my email: ">Delete My Data</a> */}
-        </div>
-      </body>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/TermsAndConditions.js
@@ -0,0 +1,96 @@
+import { Link } from 'react-router-dom';
+import './Policy.css';
+
+const TermsAndConditions = () => {
+  return (
+    <div className="privacy-policy-container">
+      <nav className="nav-home">
+        <Link to="/" className="nav-link" id="home">home</Link>
+        <Link to="/privacy-policy" className="nav-link" id="privacy-policy">privacy policy</Link>
+      </nav>
+
+      <div className="privacy-policy-content">
+        <h1>Terms & Conditions</h1>
+
+        <div className="privacy-policy-section">
+          <h2>License</h2>
+          <p>
+            By downloading or using the app, these terms will
+            automatically apply to you - you should make sure therefore
+            that you read them carefully before using the app. You are not
+            allowed to copy or modify the app, any part of the app, or
+            our trademarks in any way. You are not allowed to attempt to
+            extract the source code of the app, and you also should not try
+            to translate the app into other languages or make derivative
+            versions. The app itself, and all the trademarks, copyright,
+            database rights, and other intellectual property rights related
+            to it, still belong to "perfect pulse" App.
+          </p>
+        </div>
+
+        <div className="privacy-policy-section">
+          <h2>Changes to This Terms and Conditions</h2>
+          <p>
+            "perfect pulse" App is committed to ensuring that the app is
+            as useful and efficient as possible. For that reason, we
+            reserve the right to make changes to the app or to charge for
+            its services, at any time and for any reason. We will never
+            charge you for the app or its services without making it very
+            clear to you exactly what you are paying for.
+          </p>
+        </div>
+
+        <div className="privacy-policy-section">
+          <h2>App Responsibility</h2>
+          <p>
+            Along the same lines, "perfect pulse" App cannot always take
+            responsibility for the way you use the app i.e. You need to
+            make sure that your device stays charged - if it runs out of
+            battery and you cannot turn it on to avail the Service,
+            "perfect pulse" App cannot accept responsibility.
+          </p>
+        </div>
+
+        <div className="privacy-policy-section">
+          <h2>App Provided "As Is"</h2>
+          <p>
+            With respect to "perfect pulse" App's responsibility for your
+            use of the app, when you are using the app, it is important to
+            bear in mind that although we endeavor to ensure that it is
+            as useful and efficient as possible, you acknowledge that it
+            is provided "as is" and we cannot promise that it will be
+            without fault or interruption. You acknowledge that it is your
+            responsibility to ensure that your device meets the minimum
+            system requirements necessary to use the app.
+          </p>
+        </div>
+
+        <div className="privacy-policy-section">
+          <h2>Changes to These Terms and Conditions</h2>
+          <p>
+            We may update our Terms and Conditions
+            from time to time. Thus, you are advised to review this page
+            periodically for any changes. We will
+            notify you of any changes by posting the new Terms and
+            Conditions on this page.
+          </p>
+
+          <p>
+            These terms and conditions are effective as of October 23, 2024.
+          </p>
+        </div>
+
+        <div className="privacy-policy-section">
+          <h2>Contact Us</h2>
+          <p>
+            If you have any questions or suggestions about our
+            Terms and Conditions, do not hesitate to contact us
+            at <a href="mailto:newmodeapps@gmail.com" target="_blank" rel="noopener noreferrer">newmodeapps@gmail.com</a>.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TermsAndConditions;
